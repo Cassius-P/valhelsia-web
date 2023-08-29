485b45699156b681ecc881daf20a8d6c21731a1d
Ilias <ilias.kelesoglu@globule.net>
2023-08-29 18:27:28 +0200

    removed unused dep and useless useEffect

--- components/layouts/MainFrame.tsx
+++ components/layouts/MainFrame.tsx
@@ -1,18 +1,12 @@
-import React, {ReactNode, useEffect} from "react";
+import React, {ReactNode} from "react";
 import Sidebar from "@/components/layouts/Sidebar";
-import {Tooltip} from "@nextui-org/react";
 
 export default function MainFrame({ children }: { children: ReactNode }) {
 
-    const [isMounted, setIsMounted] = React.useState(false)
-    useEffect(() => {
-        setIsMounted(true)
-    }, []);
+ ;
 
     return (
         <>
-
-
             <Sidebar />
             <main className="flex flex-col flex-1 relative pl-16">
                 {children}
