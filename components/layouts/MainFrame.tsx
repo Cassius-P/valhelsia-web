56dbcc58384b09022bfb7352169d1ef33db7ff62
Ilias <ilias.kelesoglu@globule.net>
2023-08-29 20:35:18 +0200

    Changed color background on MainFrame

--- components/layouts/MainFrame.tsx
+++ components/layouts/MainFrame.tsx
@@ -8,7 +8,7 @@ export default function MainFrame({ children }: { children: ReactNode }) {
     return (
         <>
             <Sidebar />
-            <main className="flex flex-col flex-1 relative pl-16">
+            <main className="flex flex-col flex-1 relative pl-16 dark:bg-gray-500 bg-light-gray-200">
                 {children}
             </main>
         </>
