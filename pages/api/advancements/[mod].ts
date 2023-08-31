12c8a363ed11519f22cd163c77c3318ca21878ff
Ilias <ilias.kelesoglu@globule.net>
2023-08-31 17:27:16 +0200

    Modified query after MariaDB migration

--- pages/api/advancements/[mod].ts
+++ pages/api/advancements/[mod].ts
@@ -16,22 +16,30 @@ const GET = async (req:NextApiRequest, res:NextApiResponse) => {
     const conn = await getConnection();
 
     try {
-        const query = `SELECT
-    a.*, 
-    CASE
+        const query = `SELECT 
+    a.*,
+    CASE 
         WHEN COUNT(su_players.uid) = 0 THEN '[]'
-        ELSE JSON_ARRAYAGG(
-                JSON_OBJECT(
-                        'player_uid', su_players.uid,
-                        'player_name', su_players.name -- Assuming there's a name column in su_players
-                    )
-            )
-        END AS players
-FROM su_achievements as a
-         LEFT JOIN su_player_achievements ON a.id = su_player_achievements.achievement_id
-         LEFT JOIN su_players ON su_player_achievements.player_uid = su_players.uid
-WHERE a.mod_id = '${mod}'
-GROUP BY a.id;` ;
+        ELSE CONCAT('[', 
+            GROUP_CONCAT(
+                CONCAT(
+                    '{\"player_uid\":\"', su_players.uid, 
+                    '\", \"player_name\":\"', su_players.name, '\"}'
+                )
+            ), 
+            ']'
+        )
+    END AS players
+FROM 
+    su_achievements as a
+LEFT JOIN 
+    su_player_achievements ON a.id = su_player_achievements.achievement_id
+LEFT JOIN 
+    su_players ON su_player_achievements.player_uid = su_players.uid
+WHERE 
+    a.mod_id = '${mod}'
+GROUP BY 
+    a.id;` ;
 
 
 
