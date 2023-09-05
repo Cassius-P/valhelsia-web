590ee4ba52368d5d5a0495ee7cee41f9303c4668
Ilias <ilias.kelesoglu@globule.net>
2023-09-06 00:51:50 +0200

    Better login modal

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,26 +1,47 @@
 import {SubmitHandler, useForm} from "react-hook-form";
+import {useState} from "react";
+import {useUI} from "@/contexts/UIContext";
+import {useUser} from "@/contexts/UserContext";
 
 type LoginInputs = {
-    username: string,
+    email: string,
     password: string
 }
 
 
 const LoginModal = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm<LoginInputs>();
+    const {closeModal} = useUI()
+    const {fetchUserFromCookie} = useUser()
+
+    const [authError, setAuthError] = useState(false)
     const onSubmit: SubmitHandler<LoginInputs> = async data => {
         console.log(data)
-        const auth = await fetch('/api/auth/login', {
+
+        fetch('/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data),
         })
+        .then(async res => {
 
-        let json = await auth.json()
+            if(!res.ok) return setAuthError(true)
 
-        console.log(json)
+            let json = await res.json()
+            console.log(json)
+
+            if(json.success){
+                fetchUserFromCookie()
+                closeModal()
+            }
+
+
+        })
+        .catch(err => {
+            setAuthError(true)
+        })
     };
 
 
@@ -29,15 +50,18 @@ const LoginModal = () => {
             <h1 className={'text-center font-semibold'}>Login</h1>
             <form className={'flex flex-col space-y-2'} onSubmit={handleSubmit(onSubmit)}>
                 <div className={'flex flex-col'}>
-                    <label htmlFor={"username"} className={'font-semibold'}>Username</label>
-                    <input type="email" {...register('username', {required:true} )} className={'p-2 rounded-md outline-none focus:outline-none ring-0 focus:ring-0 border-none bg-gray-200 dark:bg-gray-600/60'}/>
+                    <label htmlFor={"email"} className={'font-semibold'}>Email</label>
+                    <input type="email" {...register('email', {required:true} )} className={'p-2 rounded-md outline-none focus:outline-none ring-0 focus:ring-0 border-none bg-gray-200 dark:bg-gray-600/60'}/>
                 </div>
                 <div className={'flex flex-col'}>
                     <label htmlFor={"password"} className={'font-semibold'}>Password</label>
                     <input type="password" {...register('password', {required:true} )} className={'p-2 rounded-md outline-none focus:outline-none ring-0 focus:ring-0 border-none bg-gray-200 dark:bg-gray-600/60'}/>
                 </div>
 
-                {(errors.password || errors.username) && <span className={"text-red-500"}>Some required fields are empty</span>}
+
+
+                {(errors.password || errors.email) && <span className={"text-red-500 font-semibold text-center"}>Some required fields are empty</span>}
+                {authError && <span className={"text-red-500 font-semibold text-center"}>Invalid credentials</span>}
 
                 <div className={'w-full pt-4'}>
                     <button type={"submit"} className={"w-full rounded-md p-2 bg-green-500 font-semibold"}>
